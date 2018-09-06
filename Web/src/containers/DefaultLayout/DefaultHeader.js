5d4d1945c084fe16b20ef96fb60efd150c4e635f
kyle zhang <skywing918@hotmail.com>
2018-09-06 13:39:06 +0800

    nav dropdown menu

--- Web/src/containers/DefaultLayout/DefaultHeader.js
+++ Web/src/containers/DefaultLayout/DefaultHeader.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import {Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink  } from 'reactstrap';
+import { Badge, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 import { AppHeaderDropdown, AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
+import DefaultHeaderDropdown from './DefaultHeaderDropdown'
 import logo from '../../assets/img/brand/logo.svg'
 import sygnet from '../../assets/img/brand/sygnet.svg'
 import { connect } from 'react-redux';
@@ -41,18 +42,15 @@ class DefaultHeader extends Component {
           </NavItem>
         </Nav>
         <Nav className="ml-auto" navbar style={{ paddingRight: '20px' }}>
-        <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-bell"></i><Badge pill color="danger">5</Badge></NavLink>
-          </NavItem>
-          <NavItem className="d-md-down-none">
-            <NavLink href="#"><i className="icon-list"></i></NavLink>
-          </NavItem>
+          <DefaultHeaderDropdown notif />
+          <DefaultHeaderDropdown tasks />
+          <DefaultHeaderDropdown mssgs />
           <AppHeaderDropdown direction="down">
             <DropdownToggle nav>
-            <img src={user.picture} className="img-avatar" alt="admin@bootstrapmaster.com" />
+              <img src={user.picture} className="img-avatar" alt="admin@bootstrapmaster.com" />
             </DropdownToggle>
             <DropdownMenu right style={{ right: 'auto' }}>
-            <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
+              <DropdownItem header tag="div" className="text-center"><strong>Account</strong></DropdownItem>
               <DropdownItem><i className="fa fa-envelope-o"></i> Messages<Badge color="success">42</Badge></DropdownItem>
               <DropdownItem><i className="fa fa-tasks"></i> Tasks<Badge color="danger">42</Badge></DropdownItem>
               <DropdownItem header tag="div" className="text-center"><strong>Settings</strong></DropdownItem>
